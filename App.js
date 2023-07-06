eb47b003f8497fdc3fc8c9be13b1b0a9118b23ad
Kalen Buchanan <Kalenbuchanan24@gmail.com>
2023-07-06 16:02:12 -0600

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import 'bootstrap/dist/css/bootstrap.css'; 
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      {}
+    </div>
+  );
+};
+
+export default App;
